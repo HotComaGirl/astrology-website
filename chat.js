fd32086db1dd8c597bd057844eb40fcafb71f90a
HotComaGirl <131263714+HotComaGirl@users.noreply.github.com>
2025-02-22 22:23:16 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                query: `Use astrology to answer my question. My astrological details are: 
+                query: `Use astrology to answer my question. Be specific and to the point. My astrological details are: 
                        DOB: ${userDOB}, Time: ${userTOB}, Place: ${userPOB}. Please remember we are in year 2025, when making future predictions.
                        My question is: ${userText}`,
                 apiKey: userAPIKey
