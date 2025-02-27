6f5f14239e56bf5652d2a6f8e3138c84afd261a3
HotComaGirl <131263714+HotComaGirl@users.noreply.github.com>
2025-02-27 11:24:36 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // API Call
         const response = await fetch("https://astrology-bot-worker.mahima-gandhi15.workers.dev/", {
             method: "POST",
-            headers: { "Authorization": 'Bearer ${userAPIKey}', "Content-Type": "text/event-stream" },
+            headers: { "Authorization": 'Bearer ${userAPIKey}', "Content-Type": "application/json" },
             body: JSON.stringify({
                 query: `1) Use astrology to answer my question with accuracy.  
                         
